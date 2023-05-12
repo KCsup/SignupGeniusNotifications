e92be18f8ece3781a5554df3e13e0bd2326d16ae
KCsup <kcsup05@gmail.com>
2023-05-12 01:46:20 -0400

    Progress on json method, currently broken.

--- src/signup.rs
+++ src/signup.rs
@@ -17,3 +17,10 @@ pub struct SignUpRole {
 }
 
 // TODO: method for getting a SignUp with the SignupGenius API
+pub fn get_json_response(
+    blocking_client: &Client,
+    url: &str,
+) -> reqwest::Result<serde_json::Value> {
+    let request = blocking_client.get("http://httpbin.org/ip").send();
+    if let Ok(response) = request {} // Will not compile, needs to return right type
+}

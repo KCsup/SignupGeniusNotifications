49289a751925a863fcad0d2b1a74ca26ea0eb3e8
KCsup <kcsup05@gmail.com>
2023-05-16 07:56:28 -0400

    Added methods for getting signup data from the SG API.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use config::Config;
 use reqwest;
-use serde_json;
 use std::collections::HashMap;
 
 mod canvas;
@@ -29,37 +28,8 @@ fn main() {
 
     println!("{:?}", settings_map);
 
-    // if let (Some(token), Some(base_url)) = (
-    //     settings_map.get(settings_keys[0]),
-    //     settings_map.get(settings_keys[1]),
-    // ) {
-    //     let resp = canvas::send_announcement(
-    //         &blocking_client,
-    //         &base_url,
-    //         &token,
-    //         6768,
-    //         "Another Rust Test, with generalized config",
-    //         "This message was sent with the Rust Programming Language, and using proper token config",
-    //         true,
-    //     );
-
-    //     if let Ok(resp_content) = resp {
-    //         println!("Status: {:?}", resp_content.status());
-    //     }
-    // }
-
-    // let json = signup::get_json_response(&blocking_client, "https://httpbin.org/json", None);
-    // println!("json: {:?}", json);
-    // if let Ok(json_value) = json {
-    //     assert_eq!(
-    //         json_value["slideshow"]["authors"],
-    //         serde_json::Value::Null,
-    //         "JSON Object Key not found..."
-    //     );
-    //     // println!("JSON Value: {:?}", json_value["slideshow"]["authors"]);
-    // }
     if let Some(sg_key) = settings_map.get("signup_genius_token") {
-        let current_ids = signup::get_active_signup_ids(&blocking_client, &sg_key);
+        let current_ids = signup::get_active_signups(&blocking_client, &sg_key);
         println!("Current IDs: {:?}", current_ids);
     }
 }
--- src/signup.rs
+++ src/signup.rs
@@ -2,19 +2,46 @@ use reqwest::blocking::Client;
 
 const BASE_SIGNUP_GENIUS_URL: &str = "https://api.signupgenius.com/v2/k";
 
+#[derive(Debug)]
 pub struct SignUp {
     pub url: String,
+    pub id: u64,
     pub title: String,
     pub author: String,
-    pub description: String,
     pub roles: Vec<SignUpRole>,
 }
 
+impl SignUp {
+    pub fn new(url: &str, id: u64, title: &str, author: &str) -> Self {
+        SignUp {
+            url: url.to_string(),
+            id,
+            title: title.to_string(),
+            author: author.to_string(),
+            roles: Vec::new(),
+        }
+    }
+
+    pub fn fill_roles(
+        &mut self,
+        blocking_client: &Client,
+        signup_genius_token: &str,
+    ) -> &Vec<SignUpRole> {
+        if let Ok(new_roles) =
+            get_signup_roles_available(blocking_client, signup_genius_token, self.id)
+        {
+            self.roles = new_roles;
+        }
+
+        &self.roles
+    }
+}
+
+#[derive(Debug)]
 pub struct SignUpRole {
     pub title: String,
-    pub current_count: u32,
-    pub needed_count: u32,
-    pub location: String,
+    pub needed_count: u64,
+    pub signup_id: u64,
     // TODO: Date and Time stuff
 }
 
@@ -33,24 +60,59 @@ pub fn get_json_response(
     request_builder.send()?.json::<serde_json::Value>()
 }
 
-// Theoretical method; Needs testing with the API to validate functionality
-pub fn get_active_signup_ids(
+pub fn get_active_signups(
     blocking_client: &Client,
     signup_genius_token: &str,
-) -> Result<Vec<u64>, reqwest::Error> {
-    let mut ids: Vec<u64> = Vec::new();
+) -> Result<Vec<SignUp>, reqwest::Error> {
+    let mut signups: Vec<SignUp> = Vec::new();
 
     if let Some(active_json_array) = get_json_response(
         blocking_client,
         &format!("{}/signups/created/active/", BASE_SIGNUP_GENIUS_URL),
         Some(vec![("user_key", signup_genius_token)]),
-    )?
-    .as_array()
+    )?["data"]
+        .as_array()
     {
         for i in 0..active_json_array.len() {
-            ids.push(active_json_array[i]["signupid"].as_u64().unwrap());
+            let signup_data = &active_json_array[i];
+            signups.push(SignUp::new(
+                signup_data["signupurl"].as_str().unwrap(),
+                signup_data["signupid"].as_u64().unwrap(),
+                signup_data["title"].as_str().unwrap(),
+                signup_data["contactname"].as_str().unwrap(),
+            ));
         }
     }
 
-    Ok(ids)
+    Ok(signups)
+}
+
+pub fn get_signup_roles_available(
+    blocking_client: &Client,
+    signup_genius_token: &str,
+    signup_id: u64,
+) -> Result<Vec<SignUpRole>, reqwest::Error> {
+    let mut roles: Vec<SignUpRole> = Vec::new();
+
+    if let Some(roles_array_json) = get_json_response(
+        blocking_client,
+        &format!(
+            "{}/signups/report/available/{}/",
+            BASE_SIGNUP_GENIUS_URL, signup_id
+        ),
+        Some(vec![("user_key", signup_genius_token)]),
+    )?["data"]["signup"]
+        .as_array()
+    {
+        for i in 0..roles_array_json.len() {
+            let role_data = &roles_array_json[i];
+            roles.push(SignUpRole {
+                title: role_data["item"].as_str().unwrap().to_string(),
+                needed_count: role_data["myqty"].as_u64().unwrap(),
+                signup_id,
+            });
+        }
+    }
+
+    Ok(roles)
 }
